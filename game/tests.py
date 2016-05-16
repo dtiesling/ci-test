03c456fa715cbcdb7ec8ac7bd4e46b4104f60538
Daniel Tiesling <tiesling@gmail.com>
2016-05-15 22:53:16 -0700

    initial commit.

---
+++ game/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+class UnitTest(TestCase):
+
+    def test_pass(self):
+
+        pass
+
+    def test_fail(self):
+
+        self.assertEqual(1, 2)
