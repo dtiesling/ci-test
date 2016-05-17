fec01cd81248b5685dc7d96a0d91854d1b2252b8
Daniel Tiesling <tiesling@gmail.com>
2016-05-16 22:38:13 -0700

    fixes style.

--- deeper/game/models.py
+++ deeper/game/models.py
@@ -4,5 +4,4 @@ from django.db import models
 class Dumb_Model(models.Model):
     # comments are dumb
 
-    title = models.CharField(max_length=255,
-         help_text='fail')
+    title = models.CharField(max_length=255, help_text='fail')
