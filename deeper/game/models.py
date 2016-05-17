7fae8b705c4edc3f6df4345b1abe7bedcba8cca2
Daniel Tiesling <tiesling@gmail.com>
2016-05-16 22:31:26 -0700

    Checking hound

--- deeper/game/models.py
+++ deeper/game/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Dumb_Model(models.Model):
+    # comments are dumb
+    title = models.CharField(max_length=255,
+         help_text='fail')
