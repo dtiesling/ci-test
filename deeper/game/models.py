c0e5a37fad50bb735b14b104a57cd116b4dcf7bf
Daniel Tiesling <tiesling@gmail.com>
2016-05-16 22:43:48 -0700

    Fail on hound violations.

--- deeper/game/models.py
+++ deeper/game/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
-
 class Dumb_Model(models.Model):
     # comments are dumb
 
-    title = models.CharField(max_length=255, help_text='fail')
+    title = models.CharField(max_length=255, help_text='failknaksndklanslkansknsjndkjsndjksn')
